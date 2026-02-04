adf28a70c15850f8b7fc04a878c6310862ad2fbb
iTTHAN <it.than@gmail.com>
2026-02-04 23:52:42 +0700

    add pynatic

--- Exam1/getEx.py
+++ Exam1/getEx.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+from typing import Union
+
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class Item(BaseModel):
+    namet: str
+    price: float
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q} 
+
+@app.post("/items/")
+def create_item(item: Item):
+    print(item.namet)
+    return {"item": item}
